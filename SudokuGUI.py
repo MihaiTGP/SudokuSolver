f1e1107264d7aa3a03daa9e60447c3f340d5166d
Mihai <62032215+MihaiTGP@users.noreply.github.com>
2021-02-10 21:35:48 +0200

    Added restrictions for letters
    The user can't input a letter, he can only input numbers from 1-9 or TAB

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from SudokuSolver import solve_sudoku, string_to_integer, integer_to_string #Importing from SudokuSolver
+from SudokuSolver import solve_sudoku, string_to_integer, integer_to_string, is_valid #Importing from SudokuSolver
 pygame.init()
 #The sudoku gird
 grid = [
@@ -19,7 +19,10 @@ HEIGHT = 900
 BLACK = (0, 0, 0)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption("Sudoku Solver")
-
+#The only keys
+VALID_KEYS = [pygame.K_1, pygame.K_2, pygame.K_3,
+              pygame.K_4, pygame.K_5, pygame.K_6,
+              pygame.K_7, pygame.K_8, pygame.K_9, pygame.K_TAB]
 dest = (0, 0)
 GRIDSIZE = 100
 GRID_WIDTH = WIDTH // GRIDSIZE
@@ -28,16 +31,16 @@ GRID_HEIGHT = HEIGHT // GRIDSIZE
 base_font = pygame.font.Font(None, 70)
 #Drawing the 4 lines that make the 3x3 squares
 def draw_lines():
-    B1 = pygame.Rect(295, 0, 10, 900)  # horizontal 1
-    B2 = pygame.Rect(595, 0, 10, 900)   # horizontal 2
-    B3 = pygame.Rect(0, 295, 900, 10)  # vertical 1
-    B4 = pygame.Rect(0, 595, 900, 10)  # vertical 2
+    line1 = pygame.Rect(295, 0, 10, 900)  # horizontal 1
+    line2 = pygame.Rect(595, 0, 10, 900)   # horizontal 2
+    line3 = pygame.Rect(0, 295, 900, 10)  # vertical 1
+    line4 = pygame.Rect(0, 595, 900, 10)  # vertical 2
 
-    pygame.draw.rect(WIN, BLACK, B1)
-    pygame.draw.rect(WIN, BLACK, B2)
-    pygame.draw.rect(WIN, BLACK, B3)
-    pygame.draw.rect(WIN, BLACK, B4)
-#Drawing the grid(9x9)
+    pygame.draw.rect(WIN, BLACK, line1)
+    pygame.draw.rect(WIN, BLACK, line2)
+    pygame.draw.rect(WIN, BLACK, line3)
+    pygame.draw.rect(WIN, BLACK, line4)
+#Drawing the grid(9x9 squares)
 def draw_grid():
     for y in range(0, int(GRID_HEIGHT)):
         for x in range(0, int(GRID_WIDTH)):
@@ -47,7 +50,6 @@ def draw_grid():
             else:
                 rr = pygame.Rect((x * GRIDSIZE, y * GRIDSIZE), (GRIDSIZE, GRIDSIZE))
                 pygame.draw.rect(WIN, (255, 255, 255), rr)
-
 
 #Drawing the grid(numbers)
 def draw_numbers(grid):
@@ -64,42 +66,36 @@ def draw_numbers(grid):
     WIN.blit(surf, (0,0))
 
 
-
-#Drawing the completed sudoku
-def solved_sudoku(grid):
-    solve_sudoku(grid)
-    integer_to_string(grid)
-    draw_grid()
-    draw_lines()
-    draw_numbers(grid)
-
 selected_cell = None
 #The main function
 def main():
-    run = True
     global grid, selected_cell
+    run = True
+    solved = False
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
             #Checking for user clicks
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and solved == False:
                 pos = pygame.mouse.get_pos()
                 col = pos[0] // GRIDSIZE
                 row = pos[1] // GRIDSIZE
                 selected_cell = [col, row]
 
             #Checking for keys pressed
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN and event.key in VALID_KEYS: #The key can only be 1,2,3..9 or TAB
                 if event.key == pygame.K_TAB: #If the key is TAB solve the puzzle
                     string_to_integer(grid)
-                    solved_sudoku(grid)
+                    solve_sudoku(grid)
+                    integer_to_string(grid)
+                    solved = True  #And don't let the user change the grid anymore
 
                 elif selected_cell != None: #Else add to the grid that number
                     user_text = event.unicode
                     grid[selected_cell[1]][selected_cell[0]] = user_text
                     selected_cell = None
-
 
         #Drawing everthing and updating the display
         draw_grid()
